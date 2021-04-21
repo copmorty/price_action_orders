798cf3dc9f05a44d039d4604d84072b8bbf445e7
Exequiel Vega <exe810@gmail.com>
2021-04-21 07:03:43 -0300

    UI window enhancement (not finished yet)

--- lib/core/globals/variables.dart
+++ lib/core/globals/variables.dart
@@ -1,7 +1,7 @@
 import 'package:price_action_orders/core/globals/constants.dart';
 import 'package:price_action_orders/core/globals/enums.dart';
 
-AppMode appMode = AppMode.PRODUCTION;
+AppMode appMode = AppMode.TEST;
 
 String binanceUrl = appMode == AppMode.PRODUCTION ? BINANCE_REAL_URL : BINANCE_TEST_URL;
 String binanceWebSocketUrl = appMode == AppMode.PRODUCTION ? BINANCE_REAL_WEBSOCKET_URL : BINANCE_TEST_WEBSOCKET_URL;
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:price_action_orders/core/globals/enums.dart';
 import 'package:price_action_orders/core/globals/variables.dart';
@@ -8,40 +9,92 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text('Price Action Orders'),
-            Expanded(child: SizedBox(width: 5)),
-            Container(
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: appMode == AppMode.PRODUCTION ? Colors.yellow : Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-              ),
-              child: Text(
-                appMode.toShortString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: appMode == AppMode.PRODUCTION ? Colors.black : Colors.white,
-                ),
+      body: Column(
+        children: [
+          WindowTitleBarBox(
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: MoveWindow(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Price Action Orders'),
+                      ),
+                    ),
+                  ),
+                  WindowButtons(),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      body: Container(
-        child: bodyBuilder(context),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Theme.of(context).primaryColor,
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/binance_logo_small.png',
+                  width: 160,
+                ),
+                Expanded(child: SizedBox(width: 5)),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: appMode == AppMode.PRODUCTION ? Colors.yellow : Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Text(
+                    appMode.toShortString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: appMode == AppMode.PRODUCTION ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                BookTickerSection(),
+                ControlsSection(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
 
-  Widget bodyBuilder(context) {
+class WindowButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Row(
       children: [
-        BookTickerSection(),
-        ControlsSection(),
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(
+          colors: closeButtonColors,
+        ),
       ],
     );
   }
 }
+
+var buttonColors = WindowButtonColors(
+  iconNormal: Colors.grey,
+  iconMouseOver: Colors.grey,
+  iconMouseDown: Colors.grey,
+  mouseOver: Color(0x22FFFFFF),
+  mouseDown: Color(0x22FFFFFF),
+);
+var closeButtonColors = WindowButtonColors(
+  iconNormal: Colors.grey,
+  mouseOver: Color(0xFFE53935),
+  mouseDown: Color(0xFFE53935),
+);
