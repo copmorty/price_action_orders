e6ce79e0d823508392e05e93209aa98e8548902d
Exequiel Vega <exe810@gmail.com>
2022-11-16 14:51:26 -0300

    Set macOS settings and window size
    .Configuration made for Flutter Secure Storage to work on macOS.
    .Changed initial window size.

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,12 +4,14 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
+
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    
+    self.setFrame(NSRect(x:0, y:0, width: 1280, height: 800), display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
   }
 }
+
